b86c827961dae5f3e8e2d00a35304452e5e3f5d7
Peter Li <peterli135@hotmail.com>
2023-07-16 21:51:19 -0700

    add api request functions

---
+++ vite-react-electron/src/utils/apiUtils.jsx
@@ -0,0 +1,19 @@
+// constant variable for base endpoints
+export const base_url = "http://127.0.0.1:8000/"
+
+
+// function for resolving API requests
+export async function resolve (promise) {
+    const response = {
+      data: null,
+      error: null
+    };
+  
+    try {
+      response.data = await promise;
+    } catch(e) {
+      response.error = e;
+    }
+  
+    return response;
+  }
