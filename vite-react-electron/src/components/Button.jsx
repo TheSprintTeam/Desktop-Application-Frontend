f4fa79aae63b2b765af2c6d3f21423a860c5e544
Peter Li <peterli135@hotmail.com>
2023-07-19 17:00:50 -0700

    reusable button component

---
+++ vite-react-electron/src/components/Button.jsx
@@ -0,0 +1,13 @@
+import "../assets/css/Button.css";
+
+export default function Button({ onClick, type, className, children}) {
+    return (
+        <button 
+            onClick={onClick}
+            type={type}
+            className={className}
+        >
+            {children}
+        </button>
+    );
+}
