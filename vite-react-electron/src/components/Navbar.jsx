7f8903b42b661f98078331e5d6f27dd6c939f060
Peter Li <peterli135@hotmail.com>
2023-07-19 17:01:19 -0700

    small fixes navbar

--- vite-react-electron/src/components/Navbar.jsx
+++ vite-react-electron/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
         <nav>
             <ul className="navbar-left">
-                <li><NavLink to={"/"}>&lt;&#47;&gt;</NavLink></li>
+                <li><NavLink to={"/"} className="sprint-icon">&lt;&#47;&gt;</NavLink></li>
                 <li><NavLink to={"tbd"}>Link 2</NavLink></li>
                 <li><NavLink to={"tbd"}>Link 3</NavLink></li>
             </ul>
