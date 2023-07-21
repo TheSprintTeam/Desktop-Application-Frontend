28cc128b4b55a544bf4aef6321d01cc2e0251130
Peter Li <peterli135@hotmail.com>
2023-07-20 19:29:34 -0700

    frontend for join team page

--- vite-react-electron/src/pages/JoinTeamPage.jsx
+++ vite-react-electron/src/pages/JoinTeamPage.jsx
@@ -1,17 +1,34 @@
+import { useState } from "react";
+
 import Button from "../components/Button";
+import "../assets/css/JoinTeamPage.css";
+import { joinTeam } from "../api/team";
 
 export default function JoinTeamPage() {
+    let [OTPCode, setOTPCode] = useState("");
 
-    function handleClick() {
-        console.log("button was click!");
+    async function handleClick() {
+        const response = await joinTeam(OTPCode);
+
+        // if response is error display pop up saying there was an error joining team, please try again:
+        if (response.error) {
+            console.log("error joining the team!");
+        } else {
+            // display that the user successfully joined the team:
+            console.log("success!");
+        }
+    }
+
+    function handleChange(e) {
+        setOTPCode(e.target.value);
     }
 
     return (
         <div className="join-team-outer">
-            <h1>Join a team</h1>
-            <p>Enter the access code provided in the box below</p>
+            <h1 className="join-team-title">Join a team</h1>
+            <p className="join-team-desc">Enter the access code provided in the box below</p>
             <div className="join-team-input-container">
-                <input type="text" className="join-team-input-field"/>
+                <input type="text" className="join-team-input-field" onChange={handleChange}/>
                 <Button onClick={handleClick} type={"button"} className={"button-submit"} children={"Join"}/>
             </div>
         </div>
