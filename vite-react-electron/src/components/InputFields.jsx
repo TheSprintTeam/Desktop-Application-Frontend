87a9a5cb772b6af21be7564d7a99d2b77bd0ae62
Peter Li <peterli135@hotmail.com>
2023-07-25 18:10:51 -0700

    add input field components

---
+++ vite-react-electron/src/components/InputFields.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa6"
+import "../assets/css/InputField.css";
+
+export function InputField({ type, className, value, placeholder, autoFocus, onChange}) {
+
+    return (
+        <input
+            type={type ? type : "text"}
+            className={className}
+            value={value}
+            placeholder={placeholder ? placeholder : ""}
+            autoFocus={autoFocus ? autoFocus : false}
+            onChange={onChange}
+        />
+    );
+}
+
+export function InputFieldPassword({ className, value, onChange }) {
+    const [passwordVisible, setPasswordVisible] = useState(false);
+
+    return (
+        <div className="password-wrapper">
+            <input 
+            type={passwordVisible ? "text" : "password"}
+            className={className}
+            value={value}
+            onChange={onChange}
+            />
+            <i className="password-visibility" onClick={e => setPasswordVisible(!passwordVisible)}>{passwordVisible ? <FaEye /> : <FaEyeSlash />}</i>
+        </div>
+    );
+}
