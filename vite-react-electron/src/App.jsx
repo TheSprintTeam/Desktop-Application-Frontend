33860df09b2d630fd9764a75998d50b23448ca6b
Peter Li <peterli135@hotmail.com>
2023-07-29 14:11:57 -0700

    connect to pages

--- vite-react-electron/src/App.jsx
+++ vite-react-electron/src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react';
 import DashboardPage from './pages/DashboardPage';
 import ErrorPage from './pages/ErrorPage';
-import SignupPage from './pages/SignupPage';
+import SignupPage from './pages/signupPage';
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Navbar from './components/Navbar.jsx';
+import Desktop2 from '../views/desktop2';
+import Desktop5 from '../views/desktop5';
+import Desktop6 from '../views/desktop6';
+import Desktop8 from '../views/desktop8';
+import Desktop9 from '../views/desktop9';
+import Desktop10 from '../views/desktop10';
 
 const router = createBrowserRouter([
   {
@@ -18,6 +24,30 @@ const router = createBrowserRouter([
       {
         path: "/login",
         element: <SignupPage />,
+      },
+      {
+        path: "/team-create-1",
+        element: <Desktop2 />,
+      },
+      {
+        path: "/team-create-2",
+        element: <Desktop5 />,
+      },
+      {
+        path: "/team-create-3",
+        element: <Desktop6 />,
+      },
+      {
+        path: "/team-create-4",
+        element: <Desktop8 />,
+      },
+      {
+        path: "/team-create-5",
+        element: <Desktop9 />,
+      },
+      {
+        path: "/team-create-6",
+        element: <Desktop10 />,
       },
     ],
   },
