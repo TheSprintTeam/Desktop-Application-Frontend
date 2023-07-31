603407d67dcf49f75969bf85b6c3746ada5b8f7b
Peter Li <peterli135@hotmail.com>
2023-07-31 15:44:00 -0700

    disable button

--- vite-react-electron/src/components/Button.jsx
+++ vite-react-electron/src/components/Button.jsx
@@ -1,12 +1,13 @@
 import "../assets/css/Button.css";
 
-export default function Button({ onClick, type, className, children}) {
+export default function Button({ onClick, type, className, disabled, children}) {
     return (
         <button 
             onClick={onClick}
             type={type}
             className={className}
             autoFocus={false}
+            disabled={disabled}
         >
             {children}
         </button>
