72ce84043f536939ffb4613f0968745ad14aae39
Peter Li <peterli135@hotmail.com>
2023-08-01 15:01:06 -0700

    add helper func check empty object

--- vite-react-electron/src/utils/helpers.jsx
+++ vite-react-electron/src/utils/helpers.jsx
@@ -1,3 +1,17 @@
 export function isEmpty(str) {
     return (!str || str.length === 0);
 }
+
+export function isEmptyObjectField(obj) {
+    for (const key in obj) {
+        if (obj[key] === "" || obj[key] === null || obj[key] == [] || obj[key] === undefined) {
+            return true;
+        }
+    }
+    return false;
+}
+
+export const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
