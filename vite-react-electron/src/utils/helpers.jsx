774b035aac5f64575b81c3638f2dd44f307ee69b
Peter Li <peterli135@hotmail.com>
2023-08-01 17:41:08 -0700

    map role names to role ids for backend

--- vite-react-electron/src/utils/helpers.jsx
+++ vite-react-electron/src/utils/helpers.jsx
@@ -14,4 +14,22 @@ export function isEmptyObjectField(obj) {
 export const validateEmail = (email) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
-  };
+};
+
+export const roles = [
+    /*{
+        id: "649e1e1445463b7a2cd13e0b",
+        role: "Team Lead",
+        permissions: "all",
+    },*/
+    {
+        id: "649e1e3e45463b7a2cd13e0c",
+        role: "Member",
+        permissions: "Limited",
+    },
+    {
+        id: "649e1e7e45463b7a2cd13e0d",
+        role: "Co-Lead",
+        permissions: "All",
+    },
+];
