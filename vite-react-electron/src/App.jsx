0cc5f24ccabb9ed884284ef5aed0d40e2ff5e13f
Peter Li <peterli135@hotmail.com>
2023-08-01 17:41:19 -0700

    user auth

--- vite-react-electron/src/App.jsx
+++ vite-react-electron/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import { getUser } from './api/user.jsx';
+import Cookies from "js-cookie";
 import Navbar from './components/Navbar.jsx';
 import DashboardPage from './pages/DashboardPage';
 import ErrorPage from './pages/ErrorPage';
@@ -10,53 +11,10 @@ import ForgotPasswordPage from './pages/ForgotPasswordPage.jsx';
 import JoinTeamPage from './pages/JoinTeamPage';
 import CreateTeamPage from './pages/CreateTeamPage.jsx';
 
-function NavbarWrapper() {
-  return (
-    <div>
-      <Navbar />
-      <Outlet />
-    </div>
-  );
-}
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <NavbarWrapper />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "/",
-        element: <DashboardPage />,
-      },
-      {
-        path: "/sign-up",
-        element: <SignupPage />,
-      },
-      {
-        path: "/login",
-        element: <LoginPage />,
-      },
-      {
-        path: "/forgot-password",
-        element: <ForgotPasswordPage />,
-      },
-      {
-        path: "/join-team",
-        element: <JoinTeamPage />,
-      },
-      {
-        path: "/create-team",
-        element: <CreateTeamPage />,
-      }
-    ],
-  },
-]);
-
 function App() {
   const [user, setUser] = useState({
     email: "",
-    role_id: "",
+    name: "",
     verified: "",
   });
 
@@ -68,7 +26,7 @@ function App() {
 
         setUser({
           email: userData.data.email,
-          role_id: userData.data.role_id,
+          name: userData.data.first_name + " " + userData.data.last_name,
           verified: userData.data.verified
         });
       }
@@ -76,6 +34,51 @@ function App() {
 
     checkUser();
   }, []);
+
+  function NavbarWrapper({ userLoggedIn }) {
+    return (
+      <div>
+        <Navbar userLoggedIn={userLoggedIn}/>
+        <Outlet />
+      </div>
+    );
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <NavbarWrapper userLoggedIn={user} />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "/",
+          element: <DashboardPage />,
+        },
+        {
+          path: "/sign-up",
+          element: <SignupPage />,
+        },
+        {
+          path: "/login",
+          element: <LoginPage />,
+        },
+        {
+          path: "/forgot-password",
+          element: <ForgotPasswordPage />,
+        },
+        {
+          path: "/join-team",
+          element: <JoinTeamPage />,
+        },
+        {
+          path: "/create-team",
+          element: <CreateTeamPage />,
+        }
+      ],
+    },
+  ]);
+
+  console.log(user);
 
   return (
     <>
