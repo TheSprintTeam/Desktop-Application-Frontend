c2e42adc84a27107d9293c5e51031acb76ccb3c4
Peter Li <peterli135@hotmail.com>
2023-08-03 13:37:28 -0700

    add accordion for teams page

--- vite-react-electron/src/components/Accordion.jsx
+++ vite-react-electron/src/components/Accordion.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
-import { FaPen, FaAngleUp, FaAngleDown } from "react-icons/fa6"
+import { useNavigate } from "react-router-dom";
+import { FaPen, FaAngleUp, FaAngleDown, FaDownload, FaUserPlus } from "react-icons/fa6"
 import "../assets/css/Accordion.css"
 
-export default function Accordion({ title, children, onChangeScreen }) {
+export function Accordion({ title, children, onChangeScreen }) {
     const [open, setOpen] = useState(false);
 
     const handleEditClick = (e) => {
@@ -38,6 +39,40 @@ export default function Accordion({ title, children, onChangeScreen }) {
                 </div>
             </div>
         </div>
+    );
+}
 
-    )
+export function AccordionTeams({ title, children, teamId }) {
+    const navigate = useNavigate();
+    const [open, setOpen] = useState(false);
+    
+    return (
+        <div className="accordion-teams">
+            <div className="accordion-container-teams">
+                <div className="accordion-header-container" onClick={() => setOpen(!open)}>
+                    <div className="accordion-left">
+                        <div className="accordion-title">
+                            {title}
+                        </div>
+                    </div>
+                    <div className="accordion-right">
+                        <div className="accordion-edit-icon install" onClick={() => navigate("/install/"+String(teamId))}>
+                            <span className="accordion-pen"><FaDownload /></span>
+                            Install
+                        </div>
+                        <div className="accordion-edit-icon invites" onClick={() => navigate("/sendInvite/"+String(teamId))}>
+                            <span className="accordion-pen"><FaUserPlus /></span>
+                            Send Invites
+                        </div>
+                        <div className="accordion-dropdown-icon">
+                            {open ? <FaAngleUp /> : <FaAngleDown />}
+                        </div>
+                    </div>
+                </div>
+                <div className={`accordion-inner-container${open ? " show" : ""}`}>
+                    {children}
+                </div>
+            </div>
+        </div>
+    );
 }
