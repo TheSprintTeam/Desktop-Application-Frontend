e089e46f494e9ddc12f34eb6a0f6ae806bfb6b9b
Peter Li <peterli135@hotmail.com>
2023-08-03 19:01:43 -0700

    fix links

--- vite-react-electron/src/components/LeftNavbar.jsx
+++ vite-react-electron/src/components/LeftNavbar.jsx
@@ -7,9 +7,9 @@ export default function LeftNavbar({ onChangeScreen }) {
                 <ul className="left-nav-vertical">
                     <li className="left-nav-item"><a className="left-nav-link" onClick={() => onChangeScreen(1)}>Project Info</a></li>
                     <li className="left-nav-item"><a className="left-nav-link">Search Engine</a></li>
-                    <li className="left-nav-item"><a className="left-nav-link">Recommendation</a></li>
-                    <li className="left-nav-item"><a className="left-nav-link" onClick={() => onChangeScreen(2)}>Invitations</a></li>
-                    <li className="left-nav-item"><a className="left-nav-link" onClick={() => onChangeScreen(3)}>Review</a></li>
+                    <li className="left-nav-item"><a className="left-nav-link" onClick={() => onChangeScreen(2)}>Recommendation</a></li>
+                    <li className="left-nav-item"><a className="left-nav-link" onClick={() => onChangeScreen(3)}>Invitations</a></li>
+                    <li className="left-nav-item"><a className="left-nav-link" onClick={() => onChangeScreen(4)}>Review</a></li>
                 </ul>
                 <ul className="left-nav-right-vertical"></ul>
             </nav>
