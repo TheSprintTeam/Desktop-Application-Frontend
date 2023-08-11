a5d5ac15d2a8cd6b8b94f8c69b214faeeb669513
Peter Li <peterli135@hotmail.com>
2023-08-11 09:11:40 -0700

    fix up send invite page

--- vite-react-electron/src/pages/SendInvitePage.jsx
+++ vite-react-electron/src/pages/SendInvitePage.jsx
@@ -1,71 +1,54 @@
 import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { inviteUserToTeam } from '../api/team';
-import { roles, mapRoleToId } from '../utils/helpers';
-import "../assets/css/SendInvite.css";
+import { mapRoleToId } from '../utils/helpers';
+import Button from '../components/Button';
+import InviteUsers from '../components/InviteUsers';
+import PopupModal from '../components/PopupModal';
 
 const SendInvitePage = () => {
-  const [inviteUser, setInviteUser] = useState({
-    email: "",
-    role: "Member",
+  let {team_id} = useParams();
+  const navigate = useNavigate();
+
+  const invitesInfo = {
+    users: []
+  }
+
+  const [invites, setInvites] = useState(invitesInfo);
+  const [modalContent, setModalContent] = useState({
+    "title": "",
+    "children": "",
+    "showModal": false,
   });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setInviteUser((prevInviteUser) => ({
-      ...prevInviteUser,
-      [name]: value,
-    }));
-  };
+  const handleSubmitInvites = () => {
+    invites.users.forEach((user) => {
+      inviteUserToTeam(team_id, user.email, mapRoleToId(user.userRole))
+    });
+    setModalContent({
+      title: "Success",
+      children: "You have successfully invited the users to your team. Redirecting you back to your teams screen in 3 seconds",
+      showModal: true
+    });
+    setTimeout(() => {
+        navigate("/view-teams");
+        window.location.reload();
+    }, 3000);
+  }
 
-  const handleSubmit = (e) => {
-    debugger;
-    e.preventDefault();
-    inviteUserToTeam(team_id, inviteUser.email, mapRoleToId(inviteUser.role));
-  };
-
-  let {team_id} = useParams();
-  
   return (
-    <div className="outer">
-      <h2 className="Title"> Send an invite</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-field">
-          <label className = "label" htmlFor="email">Email: </label>
-          <br></br>
-          <input
-          className = "input-field"
-            type="email"
-            id="email"
-            name="email"
-            value={inviteUser.email}
-            onChange={handleChange}
-            required
-          />
+    <>
+      <div className="outer-invite">
+        <h1 className="outer-invite-title">Send an invite to your team</h1>
+        <div className="view-teams-invite-container">
+          <InviteUsers invites={invites} onInvitesChange={setInvites}/>
+          <Button onClick={handleSubmitInvites} className="view-teams-invite-button" children="Send Invite"/>
         </div>
-        <div className = "form-field">
-          <label className = "label" htmlFor="role">Role: </label>
-          <br></br>
-          <select
-            id="role"
-            name="role"
-            value={inviteUser.role}
-            onChange={handleChange}
-            required
-          >
-            {roles.map((role) => (
-              <option key={role.id} value={role.role}>
-                {role.role}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className = "flex">
-        <div className ="button-outer-submit"><button class = "button" type="submit">Submit</button></div>
-        <div className ="button-outer-prev"><button class = "button" type="">Previous</button></div>
-        </div>
-      </form>
-    </div>
+      </div>
+      <PopupModal title={modalContent.title} open={modalContent.showModal} children={modalContent.children}
+        onClose={() => setModalContent({title: "", children: "", showModal: false})} 
+      />
+    </>
   );
 };
 
