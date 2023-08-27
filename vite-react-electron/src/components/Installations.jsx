19d02063174e0a6bc0a562a02edc073416d4d3df
Peter Li <peterli135@hotmail.com>
2023-08-27 12:23:57 -0700

    add members installation view

---
+++ vite-react-electron/src/components/Installations.jsx
@@ -0,0 +1,121 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import ProgressBar from "../components/ProgressBar";
+import { FaSistrix, FaSliders } from "react-icons/fa6";
+import { getBackgroundColor, mapRoleIdToName } from "../utils/helpers";
+import "../assets/css/Installations.css";
+
+// TODO:
+// component that shows the team manager installation side of things
+    // - left sidebar for the manager (pass in members and show the progress for each of them)
+    // - single component for the view installation progress (pass in technologies and the progress for each)
+
+export function InstallationsManager({ team_lead, members }) {
+    const [selectedMember, setSelectedMember] = useState({user: team_lead});
+    const [query, setQuery] = useState("");
+
+    const handleMemberSelect = (member) => {
+        setSelectedMember(member);
+    }
+    const allMembers = [{user: team_lead}, ...members];
+    console.log(allMembers);
+
+    return (
+        <div className="installation-manager-container">
+            <div className="installation-left-container">
+                <div className="searchbox-container">
+                    <input className="searchbox-members" type="text" placeholder="Search for a team member" 
+                        onChange={e => setQuery(e.target.value)}
+                    />
+                    <div className="searchbox-icon"><FaSistrix /></div>
+                </div>
+                <div className="members-dropdown-container-installation">
+                    <ul className="members-dropdown-items-installation">
+                        {allMembers && allMembers.filter(member=>(member.user.first_name.toLowerCase()+member.user.last_name.toLowerCase()).includes(query)).map((member) => {
+                            return (
+                                <li key={member.user.id} data-index={member.user.id} 
+                                    className="members-item-installations"
+                                    onClick={() => {handleMemberSelect(member)}}>
+                                    <span className="members-progress">
+                                        <span className="members-progress-circle">Circle</span>
+                                    </span>
+                                    <div className="member-right">
+                                        <div class="member-item-text">{member.user.first_name+" "+member.user.last_name}</div>
+                                        <div className="member-item-role">
+                                            {member.role ? mapRoleIdToName(member.role) : "Team Lead"}
+                                        </div>
+                                    </div>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+            </div>
+            <InstallationsUser member={selectedMember}/>
+        </div>
+    )
+}
+
+export function InstallationsUser({ member }) {
+    const progressInstallations = [
+        {
+            name: "NPM",
+            percentage: 100,
+            status: "Completed"
+        },
+        {
+            name: "Python",
+            percentage: 76,
+            status: "Pending"
+        },
+        {
+            name: "MySQL",
+            percentage: 75,
+            status: "Failed"
+        }
+    ];
+
+    return (
+        <div className="installation-right-container">
+            <div className="installation-title">{member.user.first_name+" "+member.user.last_name}</div>
+            <div className="installation-member-status-container">
+                <div className="live-installation-header">
+                    <div className="live-installation-title">Live Installation Status</div>
+                    <div className="live-installation-filter"><span className="installation-filter-icon"><FaSliders /></span>Filter</div>
+                </div>
+                <div className="live-installation-body">
+                    <table className="live-installation-table">
+                        <colgroup>
+                            <col style={{ width: "15%" }} />
+                            <col style={{ width: "20%" }} />
+                            <col style={{ width: "45%" }} />
+                        </colgroup>
+                        <tr className="live-installation-table-header">
+                            <th>No.</th>
+                            <th>Name</th>
+                            <th>Percentage</th>
+                            <th>Status</th>
+                        </tr>
+                        {progressInstallations && progressInstallations.map((installation, index) => {
+                            return (
+                                <tr key={index + 1}>
+                                    <td>{index + 1}</td>
+                                    <td>{installation.name}</td>
+                                    <td><ProgressBar value={installation.percentage} status={installation.status} /></td>
+                                    <td>
+                                        <div className="installation-status-container">
+                                            <span className="installation-circle-outer" style={{ border: "1px solid " + getBackgroundColor(installation.percentage, installation.status) }}>
+                                                <span className="installation-circle-inner" style={{ backgroundColor: getBackgroundColor(installation.percentage, installation.status) }} />
+                                            </span>
+                                            <div className="installation-status-text">{installation.status}</div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                    </table>
+                </div>
+            </div>
+        </div>
+    )
+}
--- vite-react-electron/src/pages/InstallPage.jsx
+++ vite-react-electron/src/pages/InstallPage.jsx
@@ -3,17 +3,23 @@ import { useNavigate, useParams, useLocation } from "react-router-dom";
 import { installOnTeam } from '../api/team';
 import Button from "../components/Button";
 import { Select } from "../components/Select";
-import { FaCheck } from "react-icons/fa6";
+import { InstallationsManager } from "../components/Installations";
+import { FaCheck, FaArrowLeft } from "react-icons/fa6";
 import "../assets/css/InstallPage.css";
-import ProgressBar from "../components/ProgressBar";
 
 export default function InstallPage() {
     const navigate = useNavigate();
     const { state } = useLocation();
     const members = state.team_members;
+    const team_lead = state.team_lead;
     let {team_id} = useParams();
 
+    const [screen, setScreen] = useState(1);
     const [selectedMembers, setSelectedMembers] = useState(members);
+
+    const goBack = () => {
+        navigate(-1);
+    }
 
     const handleOptionChange = (e) => {
         const userId = e.target.getAttribute("data-index");
@@ -27,56 +33,71 @@ export default function InstallPage() {
     console.log(selectedMembers);
 
     async function handleInstall() {
+        setScreen(2);
         let response = await installOnTeam(team_id);
         if (response.error) {
             console.log("error installing");
         } else {
             console.log("successfully installed");
         }
-        navigate("/");
     }
 
     function handleLater() {
         navigate("/");
     }
+    let itemInfo;
+    if (screen === 1) {
+        itemInfo = (
+            <div className="dashpage-outer">
+                <h1 className="dashpage-title">Installation options</h1>
+                <p className="dashpage-desc">Only start installation when all members have laptops open</p>
+                <div className="dashpage-inner">
+                    <div className="dashpage-create">
+                        <div className="dashpage-button-top"> 
+                        <Button className="button-invite" onClick={handleInstall} type="submit" children={"Install Now"} />
+                        </div>
+                    </div>
+                    <div className="dashpage-join">
+                        <div className="dashpage-button-top"> 
+                        <Button className="button-invite" onClick={handleLater} type="submit" children={"Install Later"} />
+                        </div>
+                    </div>
+                </div>
+                <div className="dashpage-members-container">
+                    <div className="dashpage-members-title">Select the members you want to install technology for</div>
+                    <Select size={members.length} options={selectedMembers} onOptionsChange={setSelectedMembers} children={
+                        <>
+                            {members && members.map((member) => {
+                                return (
+                                    <li key={member.user.id} data-index={member.user.id} 
+                                        className={`members-item${(selectedMembers.find(o => o.user_id === member.user.id)) ? " selected" : ""}`} 
+                                        onClick={e => {handleOptionChange(e)}}>
+                                        <span className="members-checkbox">
+                                            <span className="members-checkbox-icon"><FaCheck/></span>
+                                        </span>
+                                        <span class="member-item-text">{member.user.first_name+" "+member.user.last_name}</span>
+                                    </li>
+                                )
+                            })}
+                        </>
+                        }
+                    />
+                </div>
+            </div>
+        );
+    } else if (screen === 2) {
+        itemInfo = (
+            <div className="team-members-installations-container">
+                <Button className="signup-button-back installations" onClick={goBack} type="button" children={
+                    <div className="external-div"><span className="external-icon"><FaArrowLeft /></span>Return to Home</div>
+                }/>
+                <div className="team-memembers-installations-title">Team Members' Installations</div>
+                <InstallationsManager team_lead={team_lead} members={members} />
+            </div>
+        );
+    }
 
     return (
-        <div className="dashpage-outer">
-            <h1 className="dashpage-title">Installation options</h1>
-            <p className="dashpage-desc">Only start installation when all members have laptops open</p>
-            <div className="dashpage-inner">
-                <div className="dashpage-create">
-                    <div className="dashpage-button-top"> 
-                    <Button className="button-invite" onClick={handleInstall} type="submit" children={"Install Now"} />
-                    </div>
-                </div>
-                <div className="dashpage-join">
-                    <div className="dashpage-button-top"> 
-                    <Button className="button-invite" onClick={handleLater} type="submit" children={"Install Later"} />
-                    </div>
-                </div>
-            </div>
-            <div className="dashpage-members-container">
-                <div className="dashpage-members-title">Select the members you want to install technology for</div>
-                <Select size={members.length} options={selectedMembers} onOptionsChange={setSelectedMembers} children={
-                    <>
-                        {members && members.map((member) => {
-                            return (
-                                <li key={member.user.id} data-index={member.user.id} 
-                                    className={`members-item${(selectedMembers.find(o => o.user_id === member.user.id)) ? " selected" : ""}`} 
-                                    onClick={e => {handleOptionChange(e)}}>
-                                    <span className="members-checkbox">
-                                        <span className="members-checkbox-icon"><FaCheck/></span>
-                                    </span>
-                                    <span class="member-item-text">{member.user.first_name+" "+member.user.last_name}</span>
-                                </li>
-                            )
-                        })}
-                    </>
-                    }
-                />
-                <ProgressBar value={30} />
-            </div>
-        </div>
+        itemInfo
     );
 }
